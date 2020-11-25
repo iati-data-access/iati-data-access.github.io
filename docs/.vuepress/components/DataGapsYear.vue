93fe7b9fb2860ba70ac35fbd00d5d87657ff43d7
Mark Brough <mark.brough@gmail.com>
2020-11-25 17:04:00 +0100

    Include data gaps information

---
+++ docs/.vuepress/components/DataGapsYear.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <b-row>
+      <b-col>
+        <b-form-group
+          label="Organisation Type">
+          <b-select
+            :options="organisationTypes"
+            v-model="organisationType">
+          </b-select>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table
+          :fields="dataFields"
+          :items="filteredData">
+        </b-table>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<style>
+.theme-default-content:not(.custom) {
+  max-width: inherit;
+}
+</style>
+<script>
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+export default {
+  name: 'DataGapsYear',
+  data () {
+    return {
+      data: [],
+      organisationTypes: [],
+      organisationType: '10 - Government',
+      transactionTypeFields: []
+    }
+  },
+  computed: {
+    filteredData() {
+      return this.data.filter(item => {
+        return item.publisher_type == this.organisationType
+      })
+    },
+    dataFields() {
+      const order = {
+        'commitments': 0,
+        'disbursements': 1,
+        'budget': 2
+      }
+      var sortedTransactionTypes = this.transactionTypeFields.sort((a, b) => {
+        var [_a_type, _a_year] = a.split("_")
+        const _a_type_order = order[_a_type]
+        var [_b_type, _b_year] = b.split("_")
+        const _b_type_order = order[_b_type]
+        if (_a_type == _b_type) {
+          if (_a_year > _b_year) {
+            return 1
+          } else {
+            return -1
+          }
+        } else {
+          return (_a_type_order > _b_type_order)
+        }
+      })
+
+      return [{key: 'publisher'}].concat(
+        sortedTransactionTypes.map(item => {
+          return {
+            key: item,
+            formatter: 'valueFormatter'
+          }
+        })
+      )
+    }
+  },
+  methods: {
+    valueFormatter(value) {
+      var _value = value ? value/1000000000.0 : 0
+      _value = _value.toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2
+      })
+      return `${_value} bn`
+    }
+  },
+  async beforeMount() {
+    var data = await axios.get(`https://iati-data-access.github.io/data/summary_year.json`)
+    const thisYear = new Date().getFullYear()
+    this.data = Object.entries(data.data.summary).reduce((summary, item)=> {
+
+      var publisher_data = {
+        'publisher': item[0],
+        'publisher_type': item[1]['type'],
+        _cellVariants: {}
+      }
+      item[1]['data'].forEach(dataItem => {
+        var year, transaction_type_key
+        if (dataItem['transaction_type'] == '2 - Outgoing Commitment') {
+          var year = dataItem['year']
+          if (year > thisYear) { return }
+          var transaction_type_key = `commitments_${year}`
+        } else if (dataItem['transaction_type'] == '3 - Disbursement') {
+          var year = dataItem['year']
+          if (year > thisYear) { return }
+          var transaction_type_key = `disbursements_${year}`
+        } else if (dataItem['transaction_type'] == 'budget - Budget') {
+          var year = dataItem['year']
+          if (year < thisYear) { return }
+          var transaction_type_key = `budget_${year}`
+        } else { return }
+        if (!this.transactionTypeFields.includes(transaction_type_key)) {
+          this.transactionTypeFields.push(transaction_type_key)
+        }
+        publisher_data[transaction_type_key] = dataItem['value']
+        if (dataItem['value'] == 0) {
+          publisher_data._cellVariants[transaction_type_key] = 'warning'
+        }
+      })
+      summary.push(publisher_data)
+      return summary
+    }, [])
+    this.organisationTypes = Object.values(data.data.summary).reduce((summary, item)=> {
+      if (!summary.includes(item["type"])) {
+        summary.push(item['type'])
+      }
+      return summary
+    }, []).sort()
+  }
+}
+</script>
---
+++ docs/data-notes/README.md
@@ -0,0 +1,74 @@
+---
+title: Data Notes
+---
+
+# Data Notes
+
+As part of its 2020-2025 Strategic Plan, IATI's first objective is to drive a significant improvement in the quality of data published to IATI. As noted in the [introduction](/introduction/#_1-1-supporting-partner-country-governments-to-better-access-and-use-iati-data), recent years have seen a significant improvement in the quality of data published.
+
+Given the continuing issues around the quality of data currently published by different publishers, IATI data still needs to be handled with caution. However, it is also important to “unpack” this conversation: some publishers’ data is now of a very good quality, and with appropriate care, can be useful. The purpose of this data notes section is to provide a simple overview of publishers’ data.
+
+## 3.1. Fields in output data
+
+For each country, two files are published, containing a common set of fields for all activities. The files have been designed so that a pivot table can be added on top of the data, in order to aggregate and summarise the data in various different ways, depending on user needs.
+
+### 3.1.1. Commitments, disbursements and expenditures
+
+Column | Example
+--- | --- | ---
+IATI-Identifier | 44000-P163328
+Title | Himachal Pradesh State Roads Transformation Project
+Reporting Organisation | 44000 – World Bank
+Reporting Organisation Type | 40 – Multilateral
+Transaction Type | 2 - Outgoing Commitment
+Aid Type | C01 – Project-type interventions
+Finance Type | 410 – Aid loan excluding debt reorganisation
+Provider Organisation | 44001 – International Bank for Reconstruction and Development
+Provider Organisation Type | 40 - Multilateral
+Receiver Organisation | Himachal Pradesh Roads & Infrastructure Development Corporation
+Receiver Organisation Type |
+Country | IN - India
+Multi Country [^1] | 1
+Sector Category | 210 – Transport & Storage
+Sector | 21020 – Road Transport
+COVID 19 [^2] | 0
+Fiscal Year | FY19
+Fiscal Quarter | Q4
+Value (USD) | 47,560,000
+
+### 3.1.2. Budgets
+
+Column | Example
+--- | ---
+IATI-Identifier | 44000-P163328
+Title | Himachal Pradesh State Roads Transformation Project
+Reporting Organisation | 44000 – World Bank
+Reporting Organisation Type | 40 – Multilateral
+Transaction Type | budget - Budget
+Aid Type | C01 – Project-type interventions
+Finance Type | 410 – Aid loan excluding debt reorganisation
+Provider Organisation | 44001 – International Bank for Reconstruction and Development
+Provider Organisation Type |
+Receiver Organisation | Himachal Pradesh Roads & Infrastructure Development Corporation
+Receiver Organisation Type | 
+Country | IN - India
+Multi Country [^1] | 1
+Sector Category | 210 – Transport & Storage
+Sector | 21020 – Road Transport
+COVID 19 [^2] | 0
+Fiscal Year | FY19
+Fiscal Quarter | Q4
+Value (USD) | 435,000
+
+
+## 3.2 Data gaps by year
+
+The following table shows each publisher and the total amount of funding (in USD) found in their published data after processing, for all countries/regions. It includes the past three years (for commitments and disbursements) and the next three years (for budgets).
+
+This will identify overall data gaps that you may need to be aware of. Data gaps (where USD 0.00 was found) are higlighted in yellow.
+
+<DataGapsYear />
+
+[^1]: Signifies whether this spending was originally part of a multi-country project, and funding to this country has been calculated, based on the percentage splits of the activity between countries.
+
+[^2]: Signifies whether this spending is related to COVID-19. Only includes transactions specifically tagged with COVID-19 according to the IATI COVID-19 Publishing Guidance.
