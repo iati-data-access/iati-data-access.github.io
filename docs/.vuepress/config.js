7c7ed19bb1f3b874e9f30ae67d5ef0e28fb26b13
Mark Brough <mark.brough@gmail.com>
2021-03-15 18:12:24 +0000

    Add GTM tracking code to head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,16 +18,18 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    ['@vuepress/google-analytics',
-    {
-      'ga': 'UA-191452830'
-    }
-    ]
-  ],
   markdown: {
     plugins: [
       ['markdown-it-footnote', {}]
     ]
-  }
+  },
+  head: [
+     ['script', {}, `
+     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+     })(window,document,'script','dataLayer','G-L0T8H6MCPE');
+     `],
+  ]
 }
