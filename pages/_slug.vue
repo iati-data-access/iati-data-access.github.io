0de61caba9d8779b46d28428e430d6ab4bd7b4fc
Mark Brough <mark.brough@gmail.com>
2022-08-18 21:09:34 +0200

    Add pages to render mardkwon content

---
+++ pages/_slug.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="page in pages" :key="page.title">
+      <b-row>
+        <b-col lg="3" class="toc d-none d-lg-block">
+          <ul v-if="page">
+            <li
+              v-for="link of page.toc"
+              v-if="link.depth===2"
+              :key="link.id"
+              :class="{ 'toc2': link.depth === 2, 'toc3': link.depth === 3 }"
+            >
+              <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+            </li>
+          </ul>
+        </b-col>
+        <b-col lg="9">
+          <nuxt-content :document="page" />
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params, app, error }) {
+    const slug = params.slug || "index"
+    const pages = await $content(app.i18n.locale, slug)
+      .fetch()
+      .catch(() => {
+        error({ statusCode: 404, message: 'Page not found' })
+      })
+    return { pages }
+  },
+}
+</script>
