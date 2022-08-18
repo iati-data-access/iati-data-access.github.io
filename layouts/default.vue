a22baf3dd224b1409bc41c4dc13ebf2a2a7293c8
Mark Brough <mark.brough@gmail.com>
2022-08-18 21:09:19 +0200

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
+      <b-navbar-brand :to="localePath({path: '/'})">
+        <img src="/iati-logo.svg" /> Country Development Finance Data
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item
+            v-for="(item) in $t('sidebar')"
+            v-bind:key="item[0]"
+            :to="localePath({path: item[0]})">{{ item[1] }}</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item-dropdown :text="$t('selectText')" right>
+            <b-dropdown-item
+              v-for="locale in $t('languageOptions')"
+              v-bind:key="locale.value"
+              :to="switchLocalePath(locale.value)"
+              :active="$i18n.locale == locale.value">
+              {{ locale.text }}
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container class="page-container">
+      <Nuxt />
+    </b-container>
+  </div>
+</template>
+<style lang="scss">
+.page-container {
+  margin-top: 100px;
+}
+.navbar-brand {
+  img {
+    height: 2.2rem;
+    min-width: 2.2rem;
+    margin-right: 0.8rem;
+    vertical-align: top;
+  }
+}
+.center-logo {
+  img {
+    width: 300px;
+  }
+  text-align: center;
+}
+.nuxt-content table {
+  @extend .table !optional;
+}
+.toc2, .toc3 {
+  list-style-type: none;
+  margin-left:  0px;
+  padding-left:  0px;
+  a {
+    color: #000000;
+  }
+}
+.toc2 {
+  margin-left: 5px;
+}
+.toc3 {
+  margin-left: 10px;
+}
+</style>
+<script>
+export default {
+  async asyncData({ $content, params, app }) {
+    const page = await $content(app.i18n.locale, params.slug)
+      .fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
