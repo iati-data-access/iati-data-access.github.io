e14c8320ad50628e80762d427cbf33afaddeb839
Mark Brough <mark.brough@gmail.com>
2022-08-19 15:37:16 +0200

    Handle accents in anchors

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -22,6 +22,31 @@
   </div>
 </template>
 <script>
+import {slugify} from 'transliteration'
+
+// https://github.com/nuxt/content/issues/702
+function fixIds(elements = []) {
+  elements.forEach((el) => {
+    if (el.props?.id) {
+      el.props.id = slugify(el.props.id)
+    }
+    if (el.props?.href) {
+      const URI = decodeURI(el.props.href);
+      const httpScheme = URI.startsWith('http://');
+      const httpsScheme = URI.startsWith('https://');
+      const isExternalURI = httpScheme || httpsScheme;
+      const href = isExternalURI ? URI : '#' + slugify(URI);
+      el.props.href = href;
+    }
+    if (el.id) {
+      el.id = slugify(el.id)
+    }
+    if (el.children) {
+      fixIds(el.children)
+    }
+  })
+}
+
 export default {
   async asyncData({ $content, params, app, error }) {
     const slug = params.slug || "index"
@@ -30,7 +55,11 @@ export default {
       .catch(() => {
         error({ statusCode: 404, message: 'Page not found' })
       })
+    pages.forEach(page => {
+      fixIds(page.body.children)
+      fixIds(page.toc)
+    })
     return { pages }
-  },
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,30 @@
   </div>
 </template>
 <script>
+import {slugify} from 'transliteration'
+
+// https://github.com/nuxt/content/issues/702
+function fixIds(elements = []) {
+  elements.forEach((el) => {
+    if (el.props?.id) {
+      el.props.id = slugify(el.props.id)
+    }
+    if (el.props?.href) {
+      const URI = decodeURI(el.props.href);
+      const httpScheme = URI.startsWith('http://');
+      const httpsScheme = URI.startsWith('https://');
+      const isExternalURI = httpScheme || httpsScheme;
+      const href = isExternalURI ? URI : '#' + slugify(URI);
+      el.props.href = href;
+    }
+    if (el.id) {
+      el.id = slugify(el.id)
+    }
+    if (el.children) {
+      fixIds(el.children)
+    }
+  })
+}
 export default {
   async asyncData({ $content, params, app, error }) {
     const page = await $content(app.i18n.locale, 'index')
@@ -11,6 +35,8 @@ export default {
       .catch(() => {
         error({ statusCode: 404, message: 'Page not found' })
       })
+    fixIds(page.body.children)
+    fixIds(page.toc)
     return { page }
   },
 }
