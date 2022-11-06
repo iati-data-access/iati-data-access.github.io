e931c7b134e093f04aeb68c43f0c8e9242f93378
Mark Brough <mark.brough@gmail.com>
2022-11-06 21:38:09 +0100

    Add data index page

---
+++ pages/data/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <DataBrowserNavbar />
+    <b-jumbotron header="Data" lead="You can explore data through dashboards summarised by country, provider and sector. Try some of the examples below.">
+      <b-btn
+        :to="localePath({name: 'data-countries-code', params: { code: 'RW' }})"
+        variant="primary">Rwanda</b-btn>
+      <b-btn
+        :to="localePath({name: 'data-providers-code', params: { code: 'XM-DAC-46002' }})"
+        variant="primary">African Development Bank</b-btn>
+      <b-btn
+        :to="localePath({name: 'data-sectors-code', params: { code: '110' }})"
+        variant="primary">Education</b-btn>
+    </b-jumbotron>
+  </div>
+</template>
+<script>
+import DataBrowserNavbar from '~/components/DataBrowserNavbar'
+export default {
+  components: { DataBrowserNavbar },
+  data() {}
+}
+</script>
