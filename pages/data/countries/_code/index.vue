c9298dae8cf92567c40a1932b1f482791c9ca5d7
Mark Brough <mark.brough@gmail.com>
2022-11-06 19:53:19 +0100

    Split up visualisation into multiple pages

---
+++ pages/data/countries/_code/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <DataBrowserNavbar />
+    <h2>
+      Country dashboard for <b>{{ drilldownLabel }}</b>
+    </h2>
+    <b-row>
+      <b-col md="12" class="mt-2">
+        <DataBrowserFilter
+          :exclude-filters="['recipient_country_or_region', 'transaction_type']"
+          :setFields.sync="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="6" class="mt-2">
+        <h3>by Reporting Organisation</h3>
+        <DataBrowser
+          drilldown="reporting_organisation"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+      <b-col md="6" class="mt-2">
+        <h3>by Sector</h3>
+        <DataBrowser
+          drilldown="sector_category"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+     </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <hr />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="6" class="mt-2">
+        <h3>by Finance Type</h3>
+        <DataBrowser
+          drilldown="finance_type"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+      <b-col md="6" class="mt-2">
+        <h3>by Aid Type</h3>
+        <DataBrowser
+          drilldown="aid_type"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <hr />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="6" class="mt-2">
+        <h3>by Organisation Type</h3>
+        <DataBrowser
+          drilldown="reporting_organisation.type"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+      <b-col md="6" class="mt-2">
+        <h3>by Humanitarian / Development</h3>
+        <DataBrowser
+          drilldown="humanitarian"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <hr />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="12" class="mt-2">
+        <h3>by Activity</h3>
+        <DataBrowser
+          drilldown="activity.title"
+          :setFields="setFields"
+          :currency.sync="currency"
+         />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<style>
+
+@media (min-width: 768px) {
+  .scrollable-menu {
+      height: auto;
+      max-height: calc(100vh - 140px);
+      overflow-x: hidden;
+  }
+}
+
+@media (max-width: 767px) {
+  .scrollable-menu {
+    height: auto;
+    max-height: calc(100vh - 320px);
+    overflow-x: hidden;
+  }
+}
+</style>
+<script>
+
+import { mapState } from 'vuex'
+import DataBrowser from '~/components/DataBrowser'
+import DataBrowserFilter from '~/components/DataBrowserFilter'
+import DataBrowserNavbar from '~/components/DataBrowserNavbar'
+export default {
+  components: { DataBrowser, DataBrowserFilter },
+  data() {
+    return {
+      drilldown: 'recipient_country_or_region',
+      drilldownValue: this.$route.params.code,
+      setFields: {
+        reporting_organisation: [],
+        aid_type: [],
+        finance_type: [],
+        flow_type: [],
+        transaction_type: ['3', '4'],
+        sector_category: [],
+        sector: [],
+        recipient_country_or_region: [this.$route.params.code],
+        year: ['2020']
+      },
+      currency: 'usd'
+    }
+  },
+  computed: {
+    drilldownLabel() {
+      if (this.fields[this.drilldown].length == 0) {
+        return ''
+      }
+      return this.fields[this.drilldown].filter(drilldown => {
+        return drilldown.code === this.drilldownValue
+      })[0].label
+    },...mapState(['fields'])
+  },
+  mounted: function() {
+    this.$store.dispatch('getCodelists')
+  }
+}
+</script>
