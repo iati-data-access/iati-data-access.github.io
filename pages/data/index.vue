0f49c24495e6062843af590b29ae795e188dfcf8
Mark Brough <mark.brough@gmail.com>
2022-11-21 15:04:41 +0100

    Switch to use standard databrowser component, add filters

--- pages/data/index.vue
+++ pages/data/index.vue
@@ -3,28 +3,43 @@
     <DataBrowserNavbar />
     <h1>Summary</h1>
     <b-row>
+      <b-col>
+        <DataBrowserFilter
+          :exclude-filters="['recipient_country_or_region', 'reporting_organisation', 'sector', 'transaction_type']"
+          :setFields.sync="setFields"
+          :currency.sync="currency"
+        />
+      </b-col>
+    </b-row>
+    <b-row>
       <b-col md="6" class="mt-2">
         <h2>By country</h2>
-        <DataBrowserAggregates
-          :pageSize="null"
-          displayAs="map"
+        <DataBrowser
           drilldown="recipient_country_or_region"
+          displayAs="map"
+          :setFields="setFields"
+          :currency.sync="currency"
+          :pageSize="null"
          />
       </b-col>
       <b-col md="6" class="mt-2">
         <h2>By provider</h2>
-        <DataBrowserAggregates
-          displayAs="barChart"
+        <DataBrowser
           drilldown="reporting_organisation"
+          displayAs="barChart"
+          :setFields="setFields"
+          :currency.sync="currency"
          />
       </b-col>
     </b-row>
     <b-row>
       <b-col md="6" class="mt-2">
         <h2>By sector</h2>
-        <DataBrowserAggregates
-          displayAs="barChart"
+        <DataBrowser
           drilldown="sector_category"
+          displayAs="barChart"
+          :setFields="setFields"
+          :currency.sync="currency"
          />
       </b-col>
     </b-row>
@@ -35,12 +50,19 @@
 import { mapState } from 'vuex'
 import DataBrowserNavbar from '~/components/DataBrowserNavbar'
 import DataBrowserAggregates from '~/components/DataBrowserAggregates'
+import DataBrowser from '~/components/DataBrowser'
 export default {
-  components: { DataBrowserNavbar, DataBrowserAggregates },
+  components: { DataBrowserNavbar, DataBrowserAggregates, DataBrowser },
   data() {
+    const lastYear = new Date().getFullYear()-1
     return {
       drilldown: 'recipient_country_or_region',
-      drilldownValue: this.$route.params.code
+      drilldownValue: this.$route.params.code,
+      setFields: {
+        year: [lastYear],
+        transaction_type: ['budget']
+      },
+      currency: 'usd'
     }
   },
   computed: {
