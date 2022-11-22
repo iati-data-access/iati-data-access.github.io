daad8c3005d247642891b1e60552166e4d6a8a0f
Mark Brough <mark.brough@gmail.com>
2022-11-22 18:22:11 +0100

    Hide custom data download for now

--- components/DataBrowserNavbar.vue
+++ components/DataBrowserNavbar.vue
@@ -23,11 +23,13 @@
               ></v-select>
           </b-dropdown>
         </b-navbar-nav>
+        <!--
         <b-navbar-nav class="ml-auto">
           <b-nav-item
             link-classes="btn btn-link"
             :to="localePath({path: '/data/custom/'})">{{ $t('dataCustomDownload') }}</b-nav-item>
         </b-navbar-nav>
+        -->
       </b-collapse>
     </b-navbar>
   </div>
