464ee99676489f10437b011f79242bc8a2a37c86
Mark Brough <mark.brough@gmail.com>
2022-11-23 00:33:42 +0100

    Add country, provider and sector landing pages

---
+++ pages/data/countries/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <DataBrowserNavbar />
+    <h2>
+      Countries
+    </h2>
+    <p class="lead">Select a country to explore the data.</p>
+    <template v-if="fields.recipient_country_or_region.length >0 ">
+      <b-card-group columns>
+        <b-card
+          v-for="country in fields.recipient_country_or_region"
+          v-bind:key="country.code">
+          <nuxt-link
+          variant="link"
+          :to="localePath({
+            name: `data-countries-code`, params: { code: country.code }
+          })">{{ country.name }}</nuxt-link>
+        </b-card>
+      </b-card-group>
+    </template>
+    <template v-else>
+      <div class="text-center">
+        <b-spinner variant="secondary" />
+      </div>
+    </template>
+  </div>
+</template>
+<script>
+
+import { mapState } from 'vuex'
+import DataBrowserNavbar from '~/components/DataBrowserNavbar'
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(['fields'])
+  },
+  mounted: function() {
+    this.$store.dispatch('getCodelists')
+  }
+}
+</script>
