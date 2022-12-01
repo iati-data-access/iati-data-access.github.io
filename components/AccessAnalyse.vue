5e15ef65c6c4c4d24009a55c9c9e2af8b7a8861c
Mark Brough <mark.brough@gmail.com>
2022-12-01 20:42:31 +0000

    Add Access and Analyse component

---
+++ components/AccessAnalyse.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="float-right access-analyse-video">
+    <b-form-group
+      label="Select video"
+      label-size="sm">
+      <b-form-radio-group
+        v-model="accessAnalyse"
+        :options="$t('accessAnalyseOptions')"
+        name="radio-inline"
+        buttons
+        button-variant="outline-secondary"
+      ></b-form-radio-group>
+    </b-form-group>
+    <iframe width="100%" height="315" :src="`https://www.youtube.com/embed/${this.accessAnalyse}?controls=0`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin:0px 0px 10px 0px;"></iframe>
+  </div>
+</template>
+<style>
+.access-analyse-video {
+  margin-left: 15px;
+  min-width: 500px;
+}
+</style>
+<script>
+import axios from 'axios'
+export default {
+  name: 'AccessAnalyse',
+  data () {
+    return {
+      countries: [],
+      selectedCountry: null,
+      lastUpdated: null,
+      language: 'en',
+      accessAnalyse: null
+    }
+  },
+  computed: {
+    languageOptions() {
+      return this.$t('languageOptions')
+    }
+  },
+  async beforeMount() {
+    this.language = this.$i18n.locale
+    this.accessAnalyse = this.$t('accessAnalyseOptions')[0].value
+  }
+}
+</script>
