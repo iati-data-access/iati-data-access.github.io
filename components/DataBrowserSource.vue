3c97d1e3b5efde78d645694fb49070533f7ccd13
Mark Brough <mark.brough@gmail.com>
2023-02-05 20:21:27 +0100

    Add data browser source component

---
+++ components/DataBrowserSource.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-row>
+    <b-col
+      class="mt-4 mb-2 text-muted font-italic">
+      <hr />
+      <i18n path="dataDashboards.dataSource" tag="p">
+        <nuxt-link
+          place="methodology"
+          :to="localePath({path:'/methodology/'})">{{ $t('aboutNav[1][1]') }}</nuxt-link>
+      </i18n>
+      <i18n path="dataDashboards.dataSourceContact" tag="p">
+        <a
+          place="emailContact"
+          :href="`mailto:${$t('dataDashboards.emailContact')}`">{{ $t('dataDashboards.emailContact') }}</a>
+      </i18n>
+    </b-col>
+  </b-row>
+</template>
