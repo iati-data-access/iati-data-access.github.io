06501d5dc4999ef53297112ec20f557c81d34e23
Mark Brough <mark.brough@gmail.com>
2023-06-18 10:41:39 +0000

    Remove unused variables

--- store/index.js
+++ store/index.js
@@ -1,11 +1,6 @@
 import Vue from "vue";
 import axios from "axios";
 
-const baseURL = "https://humportal.org";
-const apiURL = "https://humportal.github.io/humportal-data";
-const analyticsURL =
-  "https://raw.githubusercontent.com/codeforIATI/IATI-Stats-public/gh-pages";
-
 export const state = () => ({
   availableDrilldowns: {
     "activity.iati_identifier": "Activity IATI Identifier",
