86315418fc04aa6b8d13c416b6faa4ae55156cae
Mark Brough <mark.brough@gmail.com>
2023-07-19 18:13:40 +0100

    Show budgets before disbursements/expenditure

--- components/DataBrowser.vue
+++ components/DataBrowser.vue
@@ -521,7 +521,7 @@ export default {
     setRollups() {
       if (JSON.stringify(this.setFields.transaction_type.sort()) == '["3","4","budget"]') {
         this.rollupBy = 'transaction_type.code'
-        this.rollupValues = [["3","4"],["budget"]]
+        this.rollupValues = [["budget"],["3","4"]]
       } else if (this.setFields.transaction_type && this.setFields.transaction_type.length>1 && (JSON.stringify(this.setFields.transaction_type.sort()) != '["3","4"]')) {
         this.rollupBy = 'transaction_type.code'
         this.rollupValues = this.setFields.transaction_type.map(item => [item])
