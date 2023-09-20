657f41c2760d4498041f7f9269e67f434db864a4
Mark Brough <mark.brough@gmail.com>
2023-09-20 23:52:40 +0000

    Move loading translation to translations filer

--- components/DataGapsYear.vue
+++ components/DataGapsYear.vue
@@ -36,7 +36,7 @@
           <template #table-busy>
             <div class="text-center text-secondary my-2">
               <b-spinner class="align-middle"></b-spinner>
-              <strong>Loading...</strong>
+              <strong>{{ $t('dataDashboards.loading') }}</strong>
             </div>
           </template>
           <template #empty="scope">
