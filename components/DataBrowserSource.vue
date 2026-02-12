280d45a7dafa84cbe6ec50cc84705ba9577de92f
Myles Lewando <myles.lewando@opendataservices.coop>
2026-02-12 14:32:05 +0000

    locales: update methodology statement to link to help docs

--- components/DataBrowserSource.vue
+++ components/DataBrowserSource.vue
@@ -4,9 +4,8 @@
       class="mt-4 mb-2 text-muted font-italic">
       <hr />
       <i18n path="dataDashboards.dataSource" tag="p">
-        <template v-slot:methodology>
-          <nuxt-link
-            :to="localePath({path:'/methodology/'})">{{ $t('aboutNav[1][1]') }}</nuxt-link>
+        <template v-slot:helpDocs>
+          <a href="https://docs.countrydata.iatistandard.org/">{{ $t('helpDocs') }}</a>
         </template>
       </i18n>
     </b-col>
